d3c50021d6dd3168a74495e43996c115f959c3be
Mikheull <mikhael.bailly1@orange.fr>
2020-02-20 19:06:11 +0100

    Revert "add: Github auth"
    This reverts commit fb1b259021198580c9b78dcc28b76ba26143210d.

--- amazon/oAuth.js
+++
@@ -1,9 +0,0 @@
-var id = {
-    amazon: {
-        clientID: process.env.AMAZON_CLIENT_ID,
-        clientSecret: process.env.AMAZON_CLIENT_SECRET,
-        callbackURL: process.env.AMAZON_CALLBACK
-    }
-};
-
-module.exports = id;
